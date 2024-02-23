bf25faaaa282cea62a07e64bff22e9bcf824a8a5
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-23 09:55:27 +0100

    feat: action

--- spec/copy_artifact_and_rename_action_spec.rb
+++ spec/copy_artifact_and_rename_action_spec.rb
@@ -1,4 +1,4 @@
-require_relative '/../lib/fastlane/plugin/copy_artifact_and_rename/actions/copy_artifact_and_rename_action.rb'
+require_relative '/../lib/fastlane/plugin/copy_artifact_and_rename/actions/copy_artifact_and_rename_action'
 
 RSpec.describe Fastlane::Actions::CopyAndRenameArtifactsAction do
   describe '#run' do
