b9a3388d170e66d7801ecd09c8fe2d6f3fb1f7cb
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-23 09:31:06 +0100

    fix: test

--- spec/copy_artifact_and_rename_action_spec.rb
+++ spec/copy_artifact_and_rename_action_spec.rb
@@ -1,20 +1,19 @@
 require_relative '../fastlane/actions/copy_and_rename_artifacts'
 
-class TestCopyArtifactAndRenameAction
-  def self.test_run
-    options = {
-      app_name: 'MyApp',
-      dist_artifacts_path: 'dist',
-      task: 'assemble',
-      build_type: 'Debug',
-      flavor: 'flavor'
-    }
+RSpec.describe Fastlane::Actions::CopyAndRenameArtifactsAction do
+  describe '#run' do
+    it 'runs the action' do
+      options = {
+        app_name: 'MyApp',
+        dist_artifacts_path: 'dist',
+        task: 'assemble',
+        build_type: 'Debug',
+        flavor: 'flavor'
+      }
 
-    allow(Fastlane::Actions::CopyAndRenameArtifactsAction).to receive(:get_app_version).and_return('1.0.0')
-    expect(Fastlane::Actions::CopyAndRenameArtifactsAction).to receive(:sh).twice
-    Fastlane::Actions::CopyAndRenameArtifactsAction.run(options)
+      allow(Fastlane::Actions::CopyAndRenameArtifactsAction).to receive(:get_app_version).and_return('1.0.0')
+      expect(Fastlane::Actions::CopyAndRenameArtifactsAction).to receive(:sh).twice
+      Fastlane::Actions::CopyAndRenameArtifactsAction.run(options)
+    end
   end
 end
-
-# Running the test
-TestCopyArtifactAndRenameAction.test_run
