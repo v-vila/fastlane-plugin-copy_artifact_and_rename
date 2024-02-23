072338a8f74bd10872fdc0a1f46067567a9e442c
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-23 10:22:42 +0100

    feat: delete spec

--- spec/copy_artifact_and_rename_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::CopyArtifactAndRenameAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).not_to notreceive(:message).with("The copy_artifact_and_rename plugin is working!")
-
-      Fastlane::Actions::CopyArtifactAndRenameAction.run(nil)
-    end
-  end
-end
